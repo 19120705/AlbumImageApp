1c6c0bcf66089eb7460968c75f2b90725200f845
19120705 <tranviettuan2k1@gmail.com>
2023-11-30 23:33:58 +0700

    add thumb

--- app/src/main/java/com/example/albumapp/fragments/FragmentPhoto.java
+++ app/src/main/java/com/example/albumapp/fragments/FragmentPhoto.java
@@ -1,19 +1,33 @@
 package com.example.albumapp.fragments;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -36,13 +50,39 @@ public class FragmentPhoto extends Fragment {
     private List<MyImage> listImages;
     private androidx.appcompat.widget.Toolbar toolbar_photo;
 
+    private ActivityResultLauncher<Intent> mActivityResultLauncher;
     private Context context;
 
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//
-//    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            // Xử lý dữ liệu trả về ở đây
+                            try {
+                                thumbnail = MediaStore.Images.Media.getBitmap(
+                                        getActivity().getApplicationContext().getContentResolver(), imageUri);
+
+                                //imageurl = getRealPathFromURI(imageUri);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+
+//                            if (requestCode == REQUEST_CODE_MULTI) {
+//                                ImageAdapter.MyAsyncTask myAsyncTask = new ImageAdapter.MyAsyncTask();
+//                                myAsyncTask.execute();
+//                                Toast.makeText(context, "Your image is hidden", Toast.LENGTH_SHORT).show();
+//                            }
+                        }
+                    }
+                }
+        );
+    }
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,@Nullable ViewGroup container,@Nullable Bundle
@@ -56,12 +96,94 @@ public class FragmentPhoto extends Fragment {
         setupRecyclerView();
 
         toolbar_photo = view.findViewById(R.id.toolbar_photo);
-        toolbar_photo.inflateMenu(R.menu.menu_top);
+        setupToolBarPhoto();
+
+
 
         return view;
     }
 
 
+    private void setupToolBarPhoto(){
+        toolbar_photo.inflateMenu(R.menu.menu_top);
+        toolbar_photo.setTitle(getContext().getResources().getString(R.string.photo));
+        toolbar_photo.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int id = item.getItemId();
+                if(id == R.id.menuSearch) {
+                    eventSearch(item);
+                }
+                else if(id == R.id.menuCamera)
+                {
+                    eventCamera();
+                }
+//                    case R.id.menuSearch:
+//
+//                    case R.id.menuCamera:
+//                        eventSearch(item);
+//                        //takenImg();
+//                        break;
+//                    case R.id.menuSearch_Advanced:
+//                        actionSearchAdvanced();
+//                        break;
+//                    case R.id.duplicateImages:
+//                        actionDuplicateImage();
+//                        break;
+//                    case R.id.menuFilter:
+//                        eventSearch(item);
+//                        Intent intent_mul = new Intent(getContext(), MultiSelectImage.class);
+//                        startActivityForResult(intent_mul, REQUEST_CODE_MULTI);
+//                        break;
+//                    case R.id.menuSettings:
+//                        eventSearch(item);
+//                        Intent intent = new Intent(getContext(), SettingsActivity.class);
+//                        startActivity(intent);
+//                }
+                return true;
+            }
+        });
+    }
+
+    public void eventSearch(@NonNull MenuItem item){}
+
+    //Camera
+    private static final int CAMERA_REQUEST = 1888;
+    private static final int MY_CAMERA_PERMISSION_CODE = 100;
+    private static final int PICTURE_RESULT = 1;
+    private Uri imageUri;
+    private String imageurl;
+    private Bitmap thumbnail;
+    private void eventCamera(){
+
+        // Kiểm tra quyền CAMERA
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED) {
+            // Quyền CAMERA đã được cấp, khởi chạy ActivityResultLauncher
+            // Đăng ký ActivityResultLauncher cho quyền CAMERA
+            ContentValues values = new ContentValues();
+            values.put(MediaStore.Images.Media.TITLE, "New Picture");
+            values.put(MediaStore.Images.Media.DESCRIPTION, "From your Camera");
+            imageUri = getActivity().getApplicationContext().getContentResolver().insert(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+            mActivityResultLauncher.launch(intent);
+        } else {
+            // Nếu không, yêu cầu quyền CAMERA
+            ActivityResultLauncher<String> requestPermissionLauncher =
+                    registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                        if (isGranted) {
+                            Toast.makeText(getActivity(), "camera permission granted", Toast.LENGTH_LONG).show();
+                            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                            mActivityResultLauncher.launch(cameraIntent);
+                        } else {
+                            Toast.makeText(getActivity(), "camera permission denied", Toast.LENGTH_LONG).show();
+                        }
+                    });
+            requestPermissionLauncher.launch(Manifest.permission.CAMERA);
+        }
+    }
     private void setupRecyclerView() {
 //        // Thiết lập RecyclerView với Adapter
         categoryAdapter = new CategoryAdapter(getContext());
@@ -96,11 +218,6 @@ public class FragmentPhoto extends Fragment {
                 }
                 categoryList.get(categoryCount).addItemToListImages(listImages.get(i));
             }
-            for(int i = 0; i< categoryCount; i++)
-            {
-                Log.e("Size list image", "categoriesCount "+i+":"+categoryList.get(i).getListImages().size() );
-            }
-            Log.e("Size list image", "categoriesList: "+categoryList.size() );
             return categoryList;
         } catch (Exception e) {
             return null;
