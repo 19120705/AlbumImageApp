39e57f0739cfeffe4aadf9f24461686d7653fb50
19120705 <tranviettuan2k1@gmail.com>
2024-01-10 20:33:29 +0700

    commit pull

--- app/src/main/java/com/example/albumapp/activities/PrivateAlbumActivity.java
+++ app/src/main/java/com/example/albumapp/activities/PrivateAlbumActivity.java
@@ -3,6 +3,7 @@ package com.example.albumapp.activities;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -124,6 +125,7 @@ public class PrivateAlbumActivity extends AppCompatActivity {
             for (int j = 0; j < allImages.size(); j++) {
                 if (Objects.equals(list.get(i), allImages.get(j).getPath())) {
                     dataImages.add(allImages.get(i));
+                    Log.e("1234567", "accessSecret: " + list.get(i));
                 }
             }
         }
